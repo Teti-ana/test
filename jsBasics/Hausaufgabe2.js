17a1e85a755ccef704d8d779b63846aa15866a0a
Tetja-na <tanjagrunertt@gmail.com>
2025-06-11 16:24:33 +0200

    CommitHausaufgabe1

--- jsBasics/Hausaufgabe2.js
+++ jsBasics/Hausaufgabe2.js
@@ -1,2 +1,11 @@
-let a = 10
-console.log (10);
+//Завдання 2: String-Verkettung und Template-Strin Конкатенація радків та шаблонний рядок
+let person1 = "Tetiana";
+let person2 = "Anna";
+//normale Verkettung
+let greeting1 = "Hallo, " + person1 + " und " + person2 + "!";
+
+console.log(greeting1);
+
+//шаблонний рядок для створення того ж вітання Vorlagenzeichenfolge zum Erstellen derselben Begrüßung
+let greeting2 = "Hallo, ${person1} und ${person2}! ";
+console.log(greeting2);
